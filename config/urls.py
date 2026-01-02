b9e8c3c0bd407cb30582b24bcd1c1887ef32ecf5
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-02 22:55:03 +0700

    Добавление изменений в проект.

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("habits.urls")),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
