46774d116628ac3a014b65e151c48edf56593288
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-08 21:47:43 +0700

    Удаление __pycache__ из репозитория

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,11 @@ TEMPLATES = [
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.parse(os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"))
+    'default': dj_database_url.parse(
+        os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
